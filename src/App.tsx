f7721f76421687e50c73dfa648c3ccae80606363
tascintra <tiago.slv@hotmail.com>
2022-10-29 14:11:24 -0300

    added margin top to text component in App

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ export function App() {
   return (
     <div className="bg-natureBlue h-screen flex items-center justify-center">
       <div className="bg-transWhite w-2/3 h-3/6 rounded-4xl flex flex-row">
-        <div className="w-3/6 h-full flex justify-center">
-          <Text size="md" className="text-gray-400 text">
+        <div className="w-3/6 h-full flex justify-center gap-6">
+          <Text size="md" className="text-gray-400 text mt-12">
             Dados Pessoais
           </Text>
         </div>
