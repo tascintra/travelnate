b417e9d0544533580fda7de6e26f140290697d87
tascintra <tiago.slv@hotmail.com>
2022-10-29 20:43:42 -0300

    adjusted sizes send button

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import "./styles/index.css"
 export function App() {
   return (
     <div className="bg-natureBlue h-screen flex items-center justify-center">
-      <form className="bg-transWhite w-2/3 h-3/6 rounded-4xl flex flex-row">
+      <form className="bg-transWhite w-2/3 h-3/6 rounded-4xl flex flex-row divide-x">
         <div className="w-3/6 h-full flex flex-col items-center justify-center gap-6">
           <Text size="md" className="text-gray-500 mb-2">
             Dados Pessoais
@@ -25,11 +25,12 @@ export function App() {
             <TextInput.Input type='text' id="cpf" pattern="\d{3}\.?\d{3}\.?\d{3}-?\d{2}" placeholder="CPF" />
           </TextInput.Root>
         </div>
+
         <div className="w-3/6 h-full flex flex-col items-center justify-center gap-6">
           <Text size="md" className="text-gray-500 mb-2">
             Destinos de Interesse
           </Text>
-          <Button type="submit" className="w-36 h-12 flex justify-center items-center gap-2">
+          <Button type="submit" className="w-32 h-12 flex justify-center items-center gap-2">
             <SendButton.Icon>
               <PaperPlaneRight />
             </SendButton.Icon>
