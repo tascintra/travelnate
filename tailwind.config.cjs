4b4ae9c9720320699a7c7458f28f01466caab986
tascintra <tiago.slv@hotmail.com>
2022-10-29 02:26:09 -0300

    tailwind colors definition

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,28 @@ module.exports = {
       md: 20,
     },
 
+    colors: {
+      white: '#FFF',
+      transWhite: '#FFFFFFf2',
+
+      gray: {
+        400: '#3B3B3B',
+        300: '#D2D2D2',
+        200: '#DDDDDD',
+        100: '#EAEAEA',
+      },
+      
+      gradient: {
+        st: '#18D2D9',
+        end: '#04BFBF',
+      },
+
+      gradientHover: {
+        st: '#70E2E6',
+        end: '#05F0F0'
+      },
+    },
+
 
     extend: {},
   },
