4d72103be12f2fa3c36a907f8145235f244e30dc
tascintra <tiago.slv@hotmail.com>
2022-10-29 16:42:00 -0300

    text input component created

---
+++ src/components/TextInput.tsx
@@ -0,0 +1,35 @@
+import { InputHTMLAttributes, ReactNode } from "react";
+import { Slot } from "@radix-ui/react-slot";
+
+export interface TextInputRootProps {
+  children: ReactNode
+}
+
+function TextInputRoot(props: TextInputRootProps) {
+  return (
+    <div className="flex items-center gap-2 h-12 py-4 px-3 rounded-4xl bg-white w-72 ring-2 ring-gray-100 shadow">
+      {props.children}
+    </div>
+  )
+}
+
+TextInputRoot.displayName = 'TextInput.Input'
+
+export interface TextInputInputProps
+  extends InputHTMLAttributes<HTMLInputElement> {}
+
+  function TextInputInput(props: TextInputInputProps) {
+    return (
+      <input 
+        className="bg-transparent flex-1 text-gray-400 text-sm placeholder:text-gray-300 outline-none"
+        {...props}
+      />
+    )
+  }
+
+TextInputInput.displayName = 'TextInput.Input'
+
+export const TextInput = {
+  Root: TextInputRoot,
+  Input: TextInputInput,
+}
